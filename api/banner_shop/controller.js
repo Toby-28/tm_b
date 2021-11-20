9b2157bd8fbae75006eeb7664472e6b18e41db2e
hushnud <you@example.com>
2021-11-20 16:20:01 +0500

    a

--- api/banner_shop/controller.js
+++ api/banner_shop/controller.js
@@ -4,7 +4,7 @@ const {patch_photo}= require('./bs.service')
 
 module.exports= {
     patch_photo: (req, res)=>{
-        patch_photo(req.file, req.params.id, (error, result)=>{
+        patch_photo(req.file.filename, req.params.id, (error, result)=>{
             if (error) {
                 console.log(error.sql+'\n'+error.sqlMessage)
                 return res.status(500).json(error)
--- api/banner_shop/router.js
+++ api/banner_shop/router.js
@@ -2,6 +2,9 @@ const router= require('express').Router(),
     {patch_photo}= require('./controller'),
     {upload}= require('../../tools/image_uploader')
 
+//
+
+
 router.patch('/:id', upload.single('banner_photo'), patch_photo)
 
 module.exports= router
